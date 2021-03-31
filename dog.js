4536af65c9134f854e8dba1d142bcc9137d4dfe0
hoadoan99 <overwatch19999@gmail.com>
2021-03-31 15:22:41 +0700

    sayHi method dog

--- dog.js
+++ dog.js
@@ -1,6 +1,12 @@
-function Dog(){
+const chalk = require('chalk');
+function Dog(name){
+	this.name = name;
 	this.stomach = [];
 }
 Dog.prototype.eat = function(cat){
 	this.stomatch.push(cat);
 }
+Dog.prototype.sayHi = function(){
+	console.log('hi, i am a dog , my name is ' +  chalk.red(this.name));
+}
+module.exports = Dog;
